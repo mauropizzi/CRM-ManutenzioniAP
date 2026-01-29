bf3f546d6c804de8abcc3652a761025d22d36833
[dyad] <m.pizzi.bs@gmail.com>
2026-01-29 09:53:52 +0100

    [dyad] Re-applied type fixes and suggested a rebuild for intervention form - wrote 3 file(s)

--- src/components/intervention-form.tsx
+++ src/components/intervention-form.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import * as z from 'zod';
+import * => z from 'zod';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import {
