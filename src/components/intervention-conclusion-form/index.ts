b28e0f14c8fdb344c9ea8c837ba2c9b07edd9f0b
[dyad] <m.pizzi.bs@gmail.com>
2026-02-01 14:36:10 +0100

    Reverted all changes back to version 7350a37e034250b7c71799e0616ae9bb72a9c99e

--- src/components/intervention-conclusion-form/index.ts
+++ src/components/intervention-conclusion-form/index.ts
@@ -1,1 +1,1 @@
-export { interventionConclusionFormSchema } from './intervention-conclusion-form'; export type { InterventionConclusionFormValues } from './intervention-conclusion-form'; export { InterventionOutcomeSection } from './intervention-outcome-section'; export { InterventionConclusionForm } from './intervention-conclusion-form';
+export { InterventionOutcomeSection } from './intervention-outcome-section';
