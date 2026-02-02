9df6f07a0aa46bb2b1258d74efdce393d7bbb800
[dyad] <m.pizzi.bs@gmail.com>
2026-02-02 15:51:16 +0100

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // swcMinify: false, // Rimosso: questa opzione non è più riconosciuta o è deprecata in Next.js 15
+};
+
+export default nextConfig;
