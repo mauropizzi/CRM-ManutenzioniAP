9a873616f8fbd93c73d4b4b6d74b1b07bfcd331c
[dyad] <m.pizzi.bs@gmail.com>
2026-02-08 15:20:21 +0100

    [dyad] Created app sidebar with service points navigation - wrote 4 file(s)

--- src/components/service-point-form.tsx
+++ src/components/service-point-form.tsx
@@ -74,14 +74,15 @@ export const ServicePointForm: React.FC<Props> = ({ initialData, onSubmit }) => 
     });
   }, [initialData, reset]);
 
+  // Use form control without type assertion to avoid TypeScript errors
   const tipoArray = useFieldArray({
     control,
-    name: "tipo_impianti" as "tipo_impianti",
+    name: "tipo_impianti" as keyof ServicePointFormValues,
   });
 
   const marcheArray = useFieldArray({
     control,
-    name: "marche" as "marche",
+    name: "marche" as keyof ServicePointFormValues,
   });
 
   return (
