afccca597c9c47c95174bcc08b8d392df1bc196d
[dyad] <m.pizzi.bs@gmail.com>
2026-02-09 15:06:03 +0100

    [dyad] wrote 1 file(s)

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -7,6 +7,7 @@ import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { ThemeToggle } from '@/components/theme-toggle';
 import {
   Menu,
   Home,
