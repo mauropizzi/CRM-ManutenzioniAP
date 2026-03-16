da4765f843bf932a1bf8d39704fba1909b91e2ee
[dyad] <m.pizzi.bs@gmail.com>
2026-03-16 10:30:18 +0100

    Updated src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ import { SystemTypeProvider } from "@/context/system-type-context";
 import { BrandProvider } from "@/context/brand-context";
 import { RuntimeErrorLogger } from "@/components/runtime-error-logger";
 import { DevDisableServiceWorker } from "@/components/dev-disable-service-worker";
+import { GlobalErrorHandler } from "@/components/global-error-handler";
 
 export const metadata: Metadata = {
   title: "Gestione Interventi",
@@ -34,6 +35,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange={true}
         >
+          <GlobalErrorHandler />
           <DevDisableServiceWorker />
           <RuntimeErrorLogger />
           <AuthProvider>
